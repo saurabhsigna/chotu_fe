49687ee0ab41f267ac4fef15e376030f405ccfb4
Abhinav Dhangar <118379707+abhinav-dhangar@users.noreply.github.com>
2024-08-03 23:30:29 +0530

    fre

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -23,46 +23,49 @@ messaging.onBackgroundMessage((payload) => {
     "[firebase-messaging-sw.js] Received background message ",
     payload
   );
-
-  // payload.fcmOptions?.link comes from our backend API route handle
-  // payload.data.link comes from the Firebase Console where link is the 'key'
-  const link = payload.fcmOptions?.link || payload.data?.link;
-
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: "./logo.png",
-    data: { url: link },
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-self.addEventListener("notificationclick", function (event) {
-  console.log("[firebase-messaging-sw.js] Notification click received.");
+// payload.fcmOptions?.link comes from our backend API route handle
+// payload.data.link comes from the Firebase Console where link is the 'key'
 
-  event.notification.close();
+// i have comment below code
+//   const link = payload.fcmOptions?.link || payload.data?.link;
 
-  // This checks if the client is already open and if it is, it focuses on the tab. If it is not open, it opens a new tab with the URL passed in the notification payload
-  event.waitUntil(
-    clients
-      // https://developer.mozilla.org/en-US/docs/Web/API/Clients/matchAll
-      .matchAll({ type: "window", includeUncontrolled: true })
-      .then(function (clientList) {
-        const url = event.notification.data.url;
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//     icon: "./logo.png",
+//     data: { url: link },
+//   };
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
 
-        if (!url) return;
+// self.addEventListener("notificationclick", function (event) {
+//   console.log("[firebase-messaging-sw.js] Notification click received.");
 
-        // If relative URL is passed in firebase console or API route handler, it may open a new window as the client.url is the full URL i.e. https://example.com/ and the url is /about whereas if we passed in the full URL, it will focus on the existing tab i.e. https://example.com/about
-        for (const client of clientList) {
-          if (client.url === url && "focus" in client) {
-            return client.focus();
-          }
-        }
+//   event.notification.close();
 
-        if (clients.openWindow) {
-          console.log("OPENWINDOW ON CLIENT");
-          return clients.openWindow(url);
-        }
-      })
-  );
-});
+//   // This checks if the client is already open and if it is, it focuses on the tab. If it is not open, it opens a new tab with the URL passed in the notification payload
+//   event.waitUntil(
+//     clients
+//       // https://developer.mozilla.org/en-US/docs/Web/API/Clients/matchAll
+//       .matchAll({ type: "window", includeUncontrolled: true })
+//       .then(function (clientList) {
+//         const url = event.notification.data.url;
+
+//         if (!url) return;
+
+//         // If relative URL is passed in firebase console or API route handler, it may open a new window as the client.url is the full URL i.e. https://example.com/ and the url is /about whereas if we passed in the full URL, it will focus on the existing tab i.e. https://example.com/about
+//         for (const client of clientList) {
+//           if (client.url === url && "focus" in client) {
+//             return client.focus();
+//           }
+//         }
+
+//         if (clients.openWindow) {
+//           console.log("OPENWINDOW ON CLIENT");
+//           return clients.openWindow(url);
+//         }
+//       })
+//   );
+// });
